980cf3263f4efea073cb94b42c41e2ac76e6952f
Piotr Luczko <piotr.luczko@gmail.com>
2023-04-01 01:59:06 +0200

    config

--- forge.config.js
+++ forge.config.js
@@ -1,27 +1,38 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    ignore: [
+      ".github",
+      "core",
+      "network",
+      ".gitignore",
+      "forge.config.js",
+      "local-create.sh",
+      "local-remove.sh",
+      "README.md",
+    ],
+  },
   rebuildConfig: {},
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
       config: {
-        setupIcon: './src/icons/app_icon.ico',
+        setupIcon: "./src/icons/app_icon.ico",
       },
     },
     {
       name: "@electron-forge/maker-deb",
       config: {
         options: {
-          icon: './src/icons/app_icon.png'
-        }
+          icon: "./src/icons/app_icon.png",
+        },
       },
     },
     {
       name: "@electron-forge/maker-rpm",
       config: {
         options: {
-          icon: './src/icons/app_icon.png'
-        }
+          icon: "./src/icons/app_icon.png",
+        },
       },
     },
   ],
