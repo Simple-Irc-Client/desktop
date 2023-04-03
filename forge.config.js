79e54bdcb35ebaeab3b2a3dfd5b27adaa7448647
Piotr Luczko <piotr.luczko@gmail.com>
2023-04-04 00:23:33 +0200

    config

--- forge.config.js
+++ forge.config.js
@@ -20,6 +20,7 @@ module.exports = {
     {
       name: "@electron-forge/maker-squirrel",
       config: {
+        id: "simple-irc-client",
         name: "Simple Irc Client",
         setupIcon: "./src/icons/app_icon.ico",
       },
