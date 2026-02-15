9f106b894be472649c45bb9dc4759c90800f1c87
Piotr Luczko <piotr.luczko@gmail.com>
2026-02-15 22:47:16 +0100

    clipboard

--- src/preload.cjs
+++ src/preload.cjs
@@ -1,5 +1,9 @@
-const { contextBridge } = require("electron");
+const { contextBridge, clipboard } = require("electron");
 
 contextBridge.exposeInMainWorld("sicDesktop", {
   platform: process.platform,
+  clipboard: {
+    readText: () => clipboard.readText(),
+    writeText: (text) => clipboard.writeText(text),
+  },
 });
